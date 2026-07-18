78353de71a97b32a3338be9fa0bd657e0ac687af
youngy00 <55174793+youngy00@users.noreply.github.com>
2026-07-18 06:37:27 +0000

    Place UI under dist/homebridge-ui

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   base: './',
   build: {
-    outDir: resolve(__dirname, '../homebridge-ui/public'),
+    outDir: resolve(__dirname, '../dist/homebridge-ui/public'),
     emptyOutDir: true,
   },
 });
